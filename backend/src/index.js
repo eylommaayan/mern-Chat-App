f6859d2532420cb340004a469ec7ae2358979aac
eylommaayan <eylonmaayan@gmail.com>
2025-09-25 03:09:14 +0300

    message

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookiesParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -17,6 +18,7 @@ const PORT = process.env.PORT || 5001;
 
 // 🧭 חיבור קבוצת הנתיבים של האימות תחת prefix קבוע
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
