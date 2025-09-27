9d6c57e16ef85e569c283fbc74a74bb9125c135e
eylommaayan <eylonmaayan@gmail.com>
2025-09-27 22:08:17 +0300

    Login & Signup Pages - Frontend 2:54

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState  } from 'react';
+import { useAuthStore } from "../store/useAuthStore";
 
 function SignUpPage() {
-  return (
-    <div>SignUpPage</div>
-  )
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    fukllName: '',
+    email:"",
+    password: '',
+  });
+
+  const { signup, isSingingUp } = useAuthStore();
+
+  const validateForm = () => {}
+  const handleChange = (e) => {
+    e.preventDefault();
+  }
+    
+
+  return <div className="min-h-screen grid lg:grid-cols-2">
+  {/*צד שמאל */}
+    <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+
+      <div className="w-full max-w-md space-y-8">
+        
+      </div>
+    </div>
+
+  </div>
+
 }
 
 export default SignUpPage
